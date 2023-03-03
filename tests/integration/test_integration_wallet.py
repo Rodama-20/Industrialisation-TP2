55da1c14c14b2c249a13938e305c6f1459ba493a
Cyrille Polier <cyrille.polier@he-arc.ch>
2023-03-03 10:56:04 +0100

    add wallet intergartion test

--- tests/integration/test_integration_wallet.py
+++ tests/integration/test_integration_wallet.py
@@ -15,4 +15,11 @@ from src.wallet import Wallet, InsufficientAmount
 # Vérifier que la balance de empty_wallet est 10
 def test_deferred_payment(empty_wallet: Wallet, wallet_with_50_balance: Wallet):
     """Tests a usecase of deferred payment and transfers between 2 wallets"""
-    assert False
+    wallet_with_50_balance.transfer(empty_wallet, 20)
+    empty_wallet.spend_cash(10)
+    empty_wallet.spend_cash(10, True)
+    wallet_with_50_balance.transfer(empty_wallet, 10)
+    assert empty_wallet.balance == 20
+    assert wallet_with_50_balance.balance == 20
+    empty_wallet.pay_deferred_payments()
+    assert empty_wallet.balance == 10
